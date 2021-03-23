bc8f22c81bd67f7dd977e498d34d3058b8baa8df
신승원 <ssw900528@gmail.com>
2021-03-23 17:04:13 +0900

    recommit

---
+++ 3DEngine/3DEngine.cpp
@@ -0,0 +1,95 @@
+﻿#include "stdafx.h"
+
+#pragma comment(lib,"winmm.lib")
+#pragma warning(disable:4996)
+
+#define WIDTH 1024
+#define HEIGHT 768
+
+const TCHAR windowClass[] = _T("MyGameWindow");
+const TCHAR title[] = _T("MyGame");
+
+HWND hWnd;
+
+int wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int main(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine)
+{
+	srand(static_cast<unsigned int>(time(NULL)));
+	return wWinMain(hInstance, hPrevInstance, lpCmdLine, 1);
+}
+
+int wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = windowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL, _T("Call to RegisterClassEx failed!"), _T("Windows Desktop Guided Tour"), NULL);
+		return 1;
+	}
+
+	hWnd = CreateWindow(windowClass, title, WS_OVERLAPPEDWINDOW
+		, CW_USEDEFAULT, CW_USEDEFAULT, WIDTH, HEIGHT + 32, NULL, NULL, hInstance, NULL);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL, _T("Call to CreateWindow failed!"), _T("Windows Desktop Guided Tour"), NULL);
+		return 1;
+	}
+
+	ShowWindow(hWnd, 1);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+		//Game event
+	case WM_KEYDOWN:
+		break;
+	case WM_KEYUP:
+		break;
+		//~Game event
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
